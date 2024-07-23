500ecc12c663d0a1a3e2ce77f7c6c667fe724636
Murilo Besson <murilo.besson@outlook.com>
2024-07-23 11:55:31 -0300

    Adicionando primeiro exercicio

---
+++ 01/cadastroRecruta.js
@@ -0,0 +1,5 @@
+let primeiroNome = prompt("Qual o seu primeiro nome?")
+let sobrenome = prompt("Agora digite o seu sobrenome")
+let campoEstudo = prompt("Qual o seu campo de estudo?")
+let idade = (2024 - prompt("Em que ano você nasceu?"))
+alert("Bem vindo recruta "+primeiroNome+"! Seu campo de estudo digitado foi "+campoEstudo+" e com base nos meus calculos você tem aproximadamente "+idade+" anos.")
