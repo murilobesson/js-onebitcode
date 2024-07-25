9cef7a563ca4d4bf1a81e43ea410ae8fa3195078
Murilo Besson <murilo.besson@outlook.com>
2024-07-25 17:07:24 -0300

    Aula 22

--- escopo_aulas/index.js
+++ escopo_aulas/index.js
@@ -1,15 +1,33 @@
-function exibirElemento(elemento, indice, array){
-    console.log({elemento, indice, array})
-}
-
-const lista = ["Maça", "Banana", "Laranja", "Limao"]
-
-// for (let i=0;i<lista.length;i++){
-//     exibirElemento(lista[i], i, lista)
+// function exibirElemento(elemento, indice, array){
+//     console.log({elemento, indice, array})
 // }
 
-lista.forEach(exibirElemento)
+// const lista = ["Maça", "Banana", "Laranja", "Limao"]
 
-lista.forEach(function(elemento, indice, array){
-    console.log({elemento,  indice, array})
-})
+// // for (let i=0;i<lista.length;i++){
+// //     exibirElemento(lista[i], i, lista)
+// // }
+
+// lista.forEach(exibirElemento)
+
+// lista.forEach(function(elemento, indice, array){
+//     console.log({elemento,  indice, array})
+// })
+
+const personagens = [
+    { nivel: 42, nome: "Thrall", raca: "Orc", classe: "Xamã" },
+    { nivel: 28, nome: "Garrosh", raca: "Orc", classe: "Guerreiro" },
+    { nivel: 35, nome: "Varok", raca: "Orc", classe: "Guerreiro" },
+    { nivel: 35, nome: "Uther", raca: "Humano", classe: "Paladino" },
+    { nivel: 26, nome: "Jaina", raca: "Humano", classe: "Maga" },
+    { nivel: 39, nome: "Tyrande", raca: "Elfo Noturno", classe: "Sacerdotisa" },
+    { nivel: 29, nome: "Muradin", raca: "Anão", classe: "Guerreiro" },
+  ]
+
+  //map
+
+  const nomes = personagens.map(function(personagem){
+    return personagem.nome
+  })
+
+  console.log(nomes)
