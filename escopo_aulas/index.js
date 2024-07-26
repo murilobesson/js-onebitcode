7ed56cbab08a58fefbd307a6b249f1e604b03921
Murilo Besson <murilo.besson@outlook.com>
2024-07-26 11:36:00 -0300

    Nova aula

--- escopo_aulas/index.js
+++ escopo_aulas/index.js
@@ -1,3 +1,13 @@
+const personagens = [
+    { nivel: 42, nome: "Thrall", raca: "Orc", classe: "Xamã" },
+    { nivel: 28, nome: "Garrosh", raca: "Orc", classe: "Guerreiro" },
+    { nivel: 35, nome: "Varok", raca: "Orc", classe: "Guerreiro" },
+    { nivel: 35, nome: "Uther", raca: "Humano", classe: "Paladino" },
+    { nivel: 26, nome: "Jaina", raca: "Humano", classe: "Maga" },
+    { nivel: 39, nome: "Tyrande", raca: "Elfo Noturno", classe: "Sacerdotisa" },
+    { nivel: 29, nome: "Muradin", raca: "Anão", classe: "Guerreiro" },
+]
+
 // function exibirElemento(elemento, indice, array){
 //     console.log({elemento, indice, array})
 // }
@@ -14,55 +24,52 @@
 //     console.log({elemento,  indice, array})
 // })
 
-const personagens = [
-    { nivel: 42, nome: "Thrall", raca: "Orc", classe: "Xamã" },
-    { nivel: 28, nome: "Garrosh", raca: "Orc", classe: "Guerreiro" },
-    { nivel: 35, nome: "Varok", raca: "Orc", classe: "Guerreiro" },
-    { nivel: 35, nome: "Uther", raca: "Humano", classe: "Paladino" },
-    { nivel: 26, nome: "Jaina", raca: "Humano", classe: "Maga" },
-    { nivel: 39, nome: "Tyrande", raca: "Elfo Noturno", classe: "Sacerdotisa" },
-    { nivel: 29, nome: "Muradin", raca: "Anão", classe: "Guerreiro" },
-]
+// //////////////////////////////////map
 
-//////////////////////////////////map
+// const nomes = personagens.map(function(personagem){
+//     return personagem.nome
+// })
 
-const nomes = personagens.map(function(personagem){
-    return personagem.nome
-})
+// console.log(nomes)
 
-console.log(nomes)
+// //////////////////////////////////filter
 
-//////////////////////////////////filter
-
-//   const orcs =  []
+// //   const orcs =  []
   
-//   for (let i = 0; i < personagens.length; i++) {
-//     if(personagens[i].raca === "Orc"){
-//         orcs.push(personagens[i])
+// //   for (let i = 0; i < personagens.length; i++) {
+// //     if(personagens[i].raca === "Orc"){
+// //         orcs.push(personagens[i])
+// //     }
+// //   }
+
+// const orcs = personagens.filter(function(personagem){
+//     return personagem.raca === "Orc"
+// })
+// console.log(orcs)
+
+// //////////////////////////////////reduce
+
+// const nivelTotal = personagens.reduce(function(valorAcumulado, personagem){
+//     return valorAcumulado+personagem.nivel
+// },0)
+
+// console.log(nivelTotal)
+
+// const racas = personagens.reduce(function(grupo, personagem){
+//     if(grupo[personagem.raca]){
+//         grupo[personagem.raca].push(personagem)
+//     }else{
+//         grupo[personagem.raca] = [personagem]
 //     }
-//   }
 
-const orcs = personagens.filter(function(personagem){
-    return personagem.raca === "Orc"
+//     return grupo
+// },{})
+
+// console.log(racas)
+
+// //////////////////////////////////sort
+
+personagens.sort(function(a, b){
+    return b.nivel - a.nivel
 })
-console.log(orcs)
-
-//////////////////////////////////reduce
-
-const nivelTotal = personagens.reduce(function(valorAcumulado, personagem){
-    return valorAcumulado+personagem.nivel
-},0)
-
-console.log(nivelTotal)
-
-const racas = personagens.reduce(function(grupo, personagem){
-    if(grupo[personagem.raca]){
-        grupo[personagem.raca].push(personagem)
-    }else{
-        grupo[personagem.raca] = [personagem]
-    }
-
-    return grupo
-},{})
-
-console.log(racas)
+console.log(personagens)
